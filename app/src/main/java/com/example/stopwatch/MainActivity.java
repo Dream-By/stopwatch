d587fb180a0f9b38fac3695150920a2d0384da48
Dream-By <dzmitry.dzianisau@gmail.com>
2020-06-11 09:44:47 +0300

    review code for stopwatch

--- app/src/main/java/com/example/stopwatch/MainActivity.java
+++ app/src/main/java/com/example/stopwatch/MainActivity.java
@@ -1,10 +1,15 @@
 package com.example.stopwatch;
 
+import android.os.Handler;
 import android.view.View;
+import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private int mSeconds = 0;
+    private boolean mIsRunning;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -13,11 +18,40 @@ public class MainActivity extends AppCompatActivity {
     }
 
     public void onStartClick(View view) {
+
+        mIsRunning = true;
     }
 
     public void onStopClick(View view) {
+
+        mIsRunning = false;
     }
 
     public void onResetClick(View view) {
+
+        mIsRunning = false;
+        mSeconds = 0;
+    }
+
+    private void runTimer(){
+        final TextView timeTextView = (TextView) findViewById(R.id.textViewTime);
+
+        final Handler handler = new Handler();
+        handler.post(new Runnable() {
+            @Override
+            public void run() {
+                int hours = mSeconds / 3600;
+                int minutes = (mSeconds % 3600) / 60;
+                int seconds = mSeconds % 60;
+
+                String time = String.format("%d,%02d,%02d",hours,minutes,seconds);
+                timeTextView.setText(time);
+
+                if (mIsRunning){
+                    mSeconds++;
+                }
+                handler.postDelayed(this,1000);
+            }
+        });
     }
 }
