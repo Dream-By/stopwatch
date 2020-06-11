4d54bcd663695cfd96466347ac07709b30aea2e8
Dream-By <dzmitry.dzianisau@gmail.com>
2020-06-11 09:33:55 +0300

    create UI for stopwatch

---
+++ app/src/main/java/com/example/stopwatch/MainActivity.java
@@ -0,0 +1,23 @@
+package com.example.stopwatch;
+
+import android.view.View;
+import androidx.appcompat.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    public void onStartClick(View view) {
+    }
+
+    public void onStopClick(View view) {
+    }
+
+    public void onResetClick(View view) {
+    }
+}
